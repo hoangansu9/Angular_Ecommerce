9cac3ee050ef0833232ded0dc34299faf2d34df6
AN SU HOANG <17DH110786@st.huflit.edu.vn>
2021-06-05 08:06:01 +0700

    fix

--- src/app/admin/books/books.component.ts
+++ src/app/admin/books/books.component.ts
@@ -30,13 +30,8 @@ export class BooksComponent implements OnInit {
       .getBooks()
       .subscribe((response) => this.handleSuccessfulResponse(response));
     this.activedRoute.queryParams.subscribe((params) => {
-      // get the url parameter named action. this can either be add or view.
       this.action = params['action'];
-      // get the parameter id. this will be the id of the book whose details
-      // are to be displayed when action is view.
-      const id = params['id'];
-      // if id exists, convert it to integer and then retrive the book from
-      // the books array
+      const id = params['id'];   
       if (id) {
         this.selectedBook = this.books.find((book) => {
           return book.id === +id;
@@ -46,17 +41,13 @@ export class BooksComponent implements OnInit {
  
   }
 
-  // we will be taking the books response returned from the database
-  // and we will be adding the retrieved
   handleSuccessfulResponse(response) {
     this.books = new Array<Book>();
-    //get books returned by the api call
     this.booksRecieved = response;
     for (const book of this.booksRecieved) {
       const bookwithRetrievedImageField = new Book();
       bookwithRetrievedImageField.id = book.id;
       bookwithRetrievedImageField.name = book.name;
-      //populate retrieved image field so that book image can be displayed
       bookwithRetrievedImageField.retrievedImage =
         'data:image/jpeg;base64,' + book.picByte;
       bookwithRetrievedImageField.author = book.author;
--- src/app/shopbook/shopbook.component.ts
+++ src/app/shopbook/shopbook.component.ts
@@ -25,9 +25,9 @@ export class ShopbookComponent implements OnInit {
     this.httpClientService
       .getBooks()
       .subscribe((response) => this.handleSuccessfulResponse(response));
-    //from localstorage retrieve the cart item
+
     let data = localStorage.getItem('cart');
-    //if this is not null convert it to JSON else initialize it as empty
+
     if (data !== null) {
       this.cartBooks = JSON.parse(data);
     } else {
@@ -35,17 +35,15 @@ export class ShopbookComponent implements OnInit {
     }
   }
 
-  // we will be taking the books response returned from the database
-  // and we will be adding the retrieved
   handleSuccessfulResponse(response) {
     this.books = new Array<Book>();
-    //get books returned by the api call
+
     this.booksRecieved = response;
     for (const book of this.booksRecieved) {
       const bookwithRetrievedImageField = new Book();
       bookwithRetrievedImageField.id = book.id;
       bookwithRetrievedImageField.name = book.name;
-      //populate retrieved image field so that book image can be displayed
+
       bookwithRetrievedImageField.retrievedImage =
         'data:image/jpeg;base64,' + book.picByte;
       bookwithRetrievedImageField.author = book.author;
@@ -56,24 +54,19 @@ export class ShopbookComponent implements OnInit {
   }
 
   addToCart(bookId) {
-    //retrieve book from books array using the book id
     let book = this.books.find((book) => {
       return book.id === +bookId;
     });
     let cartData = [];
-    //retrieve cart data from localstorage
+
     let data = localStorage.getItem('cart');
-    //prse it to json
+
     if (data !== null) {
       cartData = JSON.parse(data);
     }
-    // add the selected book to cart data
     cartData.push(book);
-    //updated the cartBooks
     this.updateCartData(cartData);
-    //save the updated cart data in localstorage
     localStorage.setItem('cart', JSON.stringify(cartData));
-    //make the isAdded field of the book added to cart as true
     book.isAdded = true;
   }
 

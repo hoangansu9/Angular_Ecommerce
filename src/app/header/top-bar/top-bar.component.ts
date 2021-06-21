535d808c64719e844bd91fb97ebe173748c53dfe
hoangansu <hoangansu9@gmail.com>
2021-06-21 14:58:51 +0700

    fix

--- src/app/header/top-bar/top-bar.component.ts
+++ src/app/header/top-bar/top-bar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.css']
+  styleUrls: ['./top-bar.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class TopBarComponent implements OnInit {
+  closeResult: string;
+  active = 1;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private modalService: NgbModal) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
-
+  openWindowLogin(content) {
+    this.modalService.open(content, { windowClass: 'login' });
+  }
 }

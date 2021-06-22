aab77873a7ea57b313612552da96d4c338798634
hoangansu <hoangansu9@gmail.com>
2021-06-22 20:27:10 +0700

    fix final

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastService } from '../service/toastService';
 
 @Component({
   selector: 'app-cart-page',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartPageComponent implements OnInit {
   cartBooks: any;
-  constructor() {}
+  constructor(
+    private modalService: NgbModal,
+    public toastService: ToastService,
+    private router: Router
+  ) {}
+  total = 0;
+  closeResult: string;
+  active = 1;
 
-  ngOnInit(): void {
+  ngOnInit() {
     let data = localStorage.getItem('cart');
 
     if (data !== null) {
@@ -17,5 +27,31 @@ export class CartPageComponent implements OnInit {
     } else {
       this.cartBooks = [];
     }
+    this.onTotal();
+  }
+  onTotal() {
+    this.cartBooks.forEach((element) => {
+      this.total += Number(element.price);
+    });
+  }
+  openWindowLogin(content) {
+    this.modalService.open(content, { windowClass: 'login' });
+  }
+  showSuccess() {
+    this.toastService.show('Đặt hàng thành công! ༼ つ ◕_◕ ༽つ', {
+      classname: 'bg-success text-light',
+      delay: 10000,
+    });
+    setTimeout(() => {
+      this.router.navigate(['/']);
+    }, 2000);
+    this.emptyCart();
+  }
+  isTemplate(toast) {
+    return toast.textOrTpl instanceof TemplateRef;
+  }
+  emptyCart() {
+    this.cartBooks = [];
+    localStorage.clear();
   }
 }
